e6d38bfae751c38eda819878e2875236ff8884e8
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-05-23 23:36:19 +0100

    Update the layout view

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,6 +2,7 @@
 
 <title>Modal Learning</title>
 <script src="https://cdn.tailwindcss.com"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 @livewireStyles
 <body class="bg-gray-200">
     {{ $slot }}
