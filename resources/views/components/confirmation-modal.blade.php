1c990c29abb38f66a571ed70c15ccf752f31dfb0
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-05-23 23:35:54 +0100

    Update the confirmation-modal

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,1 +1,18 @@
-<?php
+<div x-data="{ show: false }" x-show="show">
+    <div class="fixed inset-0 bg-gray-900 opacity-90"></div>
+    <div class="bg-white shadow-md p-4 max-w-sm h-48 m-auto rounded-md fixed inset-0">
+        <div class="flex flex-col h-full justify-between">
+            <header>
+                <h3 class="font-bold text-lg">{{ $title }}</h3>
+            </header>
+
+            <main class="mb-4">
+                <p class="">{{ $body }}</p>
+            </main>
+
+            <footer>
+                {{ $footer }}
+            </footer>
+        </div>
+    </div>
+</div>
